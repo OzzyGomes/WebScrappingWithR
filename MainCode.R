68254ae2109ffb68951d9b9bdbdc4bfef4bb4ad1
OzzyGomes <ozzygomes@outlook.com>
2023-08-26 17:36:34 -0300

    first scrapping code

--- MainCode.R
+++ MainCode.R
@@ -5,3 +5,16 @@ library("rvest")
 
 #função de leitura
 wikipedia <- read_html("https://en.wikipedia.org/wiki/Web_scraping")
+
+#busca nó de título
+wikipedia %>% html_elements("title")
+
+#busca nó de heading, mas tira parte do código HTML
+wikipedia %>% html_elements("h1") %>% html_text()
+
+#busca nó de parágrafo, mas tira parte do código HTML
+data <- wikipedia %>% html_elements("p") %>% html_text()
+
+length(data)
+
+data[2]
